7deca36ce12fef9db0ed2f3ef44f2ba40fd7d8f0
xcob <34405172+xcob@users.noreply.github.com>
2020-12-17 16:47:35 -0600

    added sound board project

---
+++ soundBoard/script.js
@@ -0,0 +1,28 @@
+const sounds = ['coins',  'typewriter', 'bomb', 'cheers', 'zombies', 'dishes']
+
+sounds.forEach(sound => {
+    const btn = document.createElement('button')
+    btn.classList.add('btn')
+
+    btn.innerText = sound
+
+    btn.addEventListener('click', () => {
+
+        stopSongs()
+
+        document.getElementById(sound).play()
+    })
+
+    document.getElementById('buttons').
+    appendChild(btn)
+})
+
+
+function stopSongs(){
+    sounds.forEach(sound => {
+        const song = document.getElementById(sound)
+
+        song.pause()
+        song.currentTime = 0
+    })
+}
